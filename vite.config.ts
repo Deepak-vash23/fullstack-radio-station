17a4add86300b4afffbbd8075ca2352da34bd149
Deepak-vash23 <deepakvashisth8282@gmail.com>
2025-03-29 13:38:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -10,6 +9,9 @@ export default defineConfig({
         additionalData: `@import "react-h5-audio-player/src/styles.scss";`
       }
     }
+  },
+  server: {
+    port: Number(process.env.PORT) || 10000, //  Use dynamic port for Render
+    host: "0.0.0.0"
   }
 });
-
